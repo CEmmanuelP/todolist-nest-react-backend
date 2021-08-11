3da013bb27c5c650bd3642ec7d046780ae6366f4
CEmmanuelP <chrisp3@daum.net>
2021-08-11 12:10:44 +0900

    create todo

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Todo {
@@ -13,4 +19,10 @@ export class Todo {
 
   @Column('boolean', { default: false })
   isDone: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
